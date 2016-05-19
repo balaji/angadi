c281e55f446d2f5b907d13aa8805ab6f9f81329c
Balaji Damodaran <damodaran.balaji@gmail.com>
2016-05-19 18:44:02 +0530

    fixing test errors

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   it 'Creates a new products' do
-    product = FactoryGirl.create(:product)
+    product = FactoryGirl.build_stubbed(:product)
     expect(product.name).to eq('Apple iPhone 6S Plus')
     expect(product.category.name).to eq('Mobile Phones')
     expect(product.brand.name).to eq('Apple')
